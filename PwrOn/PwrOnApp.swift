94e041e81bd4ecc520f958670b321c50a578186f
Philip Keller <philipjoelkeller@gmail.com>
2025-03-17 13:19:14 -0700

    Initial Commit

---
+++ PwrOn/PwrOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  PwrOnApp.swift
+//  PwrOn
+//
+//  Created by Philip Keller on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PwrOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
